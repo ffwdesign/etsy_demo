c040db5c52e7c5e26fc948d541b8cdd25ad83086
ffwdesign <eduardo@ffwdesign.com.br>
2014-10-16 01:44:33 -0300

    Implementing Devise authentication

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,4 +75,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  #Required for Devise. remento to change de Host to your actual server
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
