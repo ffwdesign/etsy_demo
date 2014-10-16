5d94fc465af38b9b8a66e0e387cb6269698867d5
ffwdesign <eduardo@ffwdesign.com.br>
2014-10-15 23:44:35 -0300

    Showing image in the home page

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,5 +1,5 @@
 class Listing < ActiveRecord::Base
-	if rials.env.development?
+	if Rails.env.development?
 		has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "missing.png"
 		validates_attachment_content_type :image, content_type: %w(image/jpeg image/jpg image/png)
 	else
