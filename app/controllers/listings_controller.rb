fa82042a5d3ffc055059aa6bb03bce1281768c58
ffwdesign <eduardo@ffwdesign.com.br>
2014-10-19 15:23:00 -0200

    Better Home page

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,7 +1,7 @@
 class ListingsController < ApplicationController
   before_action :set_listing, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate_user!, only: [:seller, :new, :create, :edit, :update, :destroy]
-  before_action :check_user, only: [:edit, :update, :destroy]
+  before_filter :authenticate_user!, only: [:seller, :new, :create, :edit, :update, :destroy]
+  before_filter :check_user, only: [:edit, :update, :destroy]
 
   def seller
     @listings = Listing.where(user: current_user).order("created_at DESC")
