188a134b4c372753ff9108c4109a2d4612a93ecb
ffwdesign <eduardo@ffwdesign.com.br>
2014-10-21 16:56:33 -0200

    layout

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,1 +1,9 @@
+var jumboHeight = $('.myjumbotron').outerHeight();
+function parallax(){
+    var scrolled = $(window).scrollTop();
+    $('.mybg').scss('height', (jumboHeight-scrolled) + 'px');
+}
 
+$(window).scroll(function(e){
+    parallax();
+});
